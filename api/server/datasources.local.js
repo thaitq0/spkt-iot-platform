40bf9a551e8079d40887b8f39406ebdfc2736b0e
Thai Truong <thaitq@uiza.io>
2018-08-22 00:50:23 +0700

    add db config

--- api/server/datasources.local.js
+++ api/server/datasources.local.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var mongodbURI = process.env.MONGO_URL || process.env.MONGODB_URI;
+mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+var mongodbURI = 'mongodb://myUserAdmin:abc123@172.17.0.14:27017'
+//process.env.MONGO_URL || process.env.MONGODB_URI;
 
 if (mongodbURI) {
   console.log('Data sources: Using MongoDB config', mongodbURI);
